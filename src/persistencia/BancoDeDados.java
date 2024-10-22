6f83a22cb129a4c3abde013217bbecb50ae3741a
Mona Lisa <monalisa@gmail.com>
2024-10-22 11:01:12 -0300

    Versão inicial

---
+++ src/persistencia/BancoDeDados.java
@@ -0,0 +1,31 @@
+package persistencia;
+
+
+    import java.sql.Connection;
+    import java.sql.DriverManager;
+    import java.sql.SQLException;
+
+    public class BancoDeDados {
+        private static Connection bd = null;
+
+        public static Connection getBd() {
+            if(bd == null){
+
+                try{
+                    bd = DriverManager.getConnection(
+                        "jdbc:mysql://localhost:3306/bd_imobiliaria",
+						"root",
+						"root"
+                    );
+
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                    System.exit(1);
+                }
+            }
+            return bd;
+
+        }
+    }
+
+
