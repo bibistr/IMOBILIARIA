232615896dc35deff201809997487c0589f626eb
Mona Lisa <monalisa@gmail.com>
2024-10-22 11:58:51 -0300

    Criação das classe cliente e corretor(versão s/ arrayList

---
+++ src/model/Cliente.java
@@ -0,0 +1,46 @@
+package model;
+
+class Cliente extends Usuario{
+
+ private String data_nasc;
+ private String cpf;
+ private String endereco;
+
+    public Cliente(String nome_cliente, String email_cliente, String tel_cliente, String data_nasc, String cpf, String endereco){
+        super(nome_cliente, email_cliente, tel_cliente);
+        this.data_nasc = data_nasc;
+        this.cpf = cpf;
+        this.endereco = endereco;
+    }
+    
+
+    public String getData_nasc() {
+        return data_nasc;
+    }
+    public void setData_nasc(String data_nasc) {
+        this.data_nasc = data_nasc;
+    }
+
+
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+
+    public String getEndereco() {
+        return endereco;
+    }
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    @Override
+    public String toString() {
+        return "" + getNome() + ", " + getData_nasc() + ", " + getCpf() + ", " + getEndereco() + ", " + getEmail() + ", " + getTel();
+    }
+
+    
+}
---
+++ src/model/Corretor.java
@@ -0,0 +1,24 @@
+package model;
+
+ class Corretor extends Usuario{
+
+    private String creci;
+
+    Corretor(String nome_corretor, String email_corretor, String tel_corretor, String creci){
+        super(nome_corretor,email_corretor, tel_corretor);
+        this.creci = creci;
+    }
+
+    public String getCreci() {
+        return creci;
+    }
+    public void setCreci(String creci) {
+        this.creci = creci;
+    }
+
+    @Override
+    public String toString() {
+        return "" + getNome() + ", " + getCreci() + ", E-mail: " + getEmail() + ", Telefone: " + getTel();
+    }
+
+}
