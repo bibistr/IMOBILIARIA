91ab39ff5878477667c43731037b76c28acc7f22
Luiza Demoliner <luizademoliner.ss018@academico.ifsul.edu.br>
2024-11-01 21:15:45 -0300

    Implementação CRUD na Classe ClienteDAO

--- src/persistencia/ClienteDAO.java
+++ src/persistencia/ClienteDAO.java
@@ -35,8 +35,66 @@ public class ClienteDAO{
         }
     }
 
+
+  //UPDATE 
+    public update(Cliente c) throws SQLException {
+        String query = """
+        UPDATE ciente
+        SET nome  = ?, endereco = ?, tel = ?, email = ?
+        WHERE cpf = ?
+        """;
+    
+    PreparedStatement st = this.bd,prepareStatement(query);
+    st.setString(1, c.getNome());
+    st.setString(2, c.getEndereco());
+    st.setString(3, c.getEmail());
+    st.setString(4, c.getTel());
+    st.setString(5, c.getCpf());
+    st.executeUpdate();
+    }
+
+// READ/CONSULTA
+    public ArrayList<Cliente>findByNomeLike(String n) throws SQLException {
+        ArrayList<Cliente> lista = new ArrayList<Cliente>();
+        String query = """
+               SELECT endereco, email, tel, data_nasc, cpf
+               FROM cliente
+               WHERE nome LIKE ?"
+               """;
+
+               PreparedStatement st = this.prepareStatement(query);
+               st.setString(1, "%" + n + "%")
+               ResultSet res = st.executeQuery();
+               while(res.next()) {
+                String nome = res.getString("nome");
+                String endereco = res.getString("endereco")
+                String email = res.getString("email");
+                String tel = res.getString("tel");
+                String data_nasc = res.getString("data_nasc");
+                String cpf = res.getString("cpf");
+                Cliente p = new Cliente(nome,endereco,email, tel, data_nasc, cpf);
+                lista.add()
+               }
+               return lista;
+    }
+
+
+    // DELETE
+    public void delete(Cliente c) throws SQLException {
+        String query """
+        DELETE FROM cliente
+        WHERE cpf = ?
+        """;
+        PreparedStatement st = this.bd.prepareStatement(query);
+        st.setString(1, c.getCpf());
+        st.executeUpdate();
+    }
+
+    
+
+    //LISTA GERAL
     public ArrayList<Cliente> getAll() throws SQLException {
-        ArrayList<Cliente> lista = new ArrayList<>();
+        ArrayList<Cliente> lista_geral = new ArrayList<>();
         String query = "SELECT id_cliente, nome, email, tel, data_nasc, cpf, endereco FROM cliente";
         PreparedStatement st = this.bd.prepareStatement(query);
         ResultSet res = st.executeQuery();
@@ -49,9 +107,8 @@ public class ClienteDAO{
             String cpf =  res.getString("cpf");
             String endereco = res.getString("endereco");
             Cliente c = new Cliente(id_cliente, nome_cliente, email_cliente, tel_cliente, data_nasc, cpf, endereco);
-            lista.add(c);
+            lista_geral.add(c);
         }
-        return lista;
+        return lista_geral;
     }
-
 }
